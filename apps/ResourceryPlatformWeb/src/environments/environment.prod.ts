356ce04bf961a9c87ca797d15a0c857baf991429
Oluropo Akinbolade-Jones <oakinbolade-jones@ecowas.int>
2026-06-24 16:34:53 +0100

    Stable Staging

--- apps/ResourceryPlatformWeb/src/environments/environment.prod.ts
+++ apps/ResourceryPlatformWeb/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 import { Environment } from '@abp/ng.core';
 
-const baseUrl = 'https://smartserve.ecowas.int';
+const baseUrl = 'http://smartserve.ecowas.int';
 
 export const environment = {
   production: true,
@@ -10,7 +10,7 @@ export const environment = {
     logoUrl: '',
   },
   oAuthConfig: {
-    issuer: 'https://auth.smartserve.ecowas.int/',
+    issuer: 'https://auth.smartserve.ecowas.int',
     redirectUri: baseUrl,
     clientId: 'ResourceryPlatformWorkflow_Web',
     clientSecret: '1q2w3e*',
